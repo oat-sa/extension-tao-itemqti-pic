e223596d3b1165b6252801d49e2284265991be09
ssipasseuth <sam@taotesting.com>
2014-10-10 16:43:47 +0200

    hook started

--- views/js/picManager/hook.js
+++ views/js/picManager/hook.js
@@ -1,70 +1,52 @@
 define([
     'i18n',
-    'taoQtiItem/qtiCreator/editor/interactionsToolbar',
-    'qtiItemPci/pciManager/pciManager',
-    'tpl!qtiItemPci/pciManager/tpl/managerTrigger',
     'helpers',
-    'css!qtiItemPci_css/pci-manager'
-], function(__, interactionsToolbar, PciManager, triggerTpl, helpers){
+    'tpl!qtiItemPic/picManager/tpl/manager',
+    'css!qtiItemPic_css/pic-manager'
+], function(__, helpers, managerTpl){
 
     var _urls = {
-        addRequiredResources : helpers._url('addRequiredResources', 'PciManager', 'qtiItemPci')
+        addRequiredResources : helpers._url('addRequiredResources', 'PicManager', 'qtiItemPic')
     };
 
-    function addManagerButton($interactionSidebar, $modalContainer){
-
-        //get the custom interaction section in the toolbar
-        var customInteractionTag = interactionsToolbar.getCustomInteractionTag();
-        var $section = interactionsToolbar.getGroup($interactionSidebar, customInteractionTag);
-        if(!$section.length){
-            //no custom interaction yet, add a section
-            $section = interactionsToolbar.addGroup($interactionSidebar, customInteractionTag);
-        }
-
-        //add button
-        var $button = $(triggerTpl({
-            title : __('Manage custom interactions')
-        }));
-        $section.children('.panel').append($button);
-
-        var pciManager = new PciManager({
-            container : $modalContainer,
-            interactionSidebar : $interactionSidebar
-        });
-        $button.on('click', function(){
-            pciManager.open();
-        });
-    }
-    
-    function addNewInteractionListener(uri){
-        //listener:
-        $(document).on('elementCreated.qti-widget.pci-hook', function(e, data){
-            var element = data.element;
-            if(element.qtiClass === 'customInteraction'){
-                $.getJSON(_urls.addRequiredResources, {typeIdentifier : element.typeIdentifier, uri:uri});
+    function addStudentToolManager($itemPropPanel){
+        
+        var tools = {
+            studentTool1 : {
+                label : 'tool 1',
+                description : 'this is a tool 1',
+                checked : false
+            },
+            studentTool2 : {
+                label : 'tool 2',
+                description : 'this is a tool 2',
+                checked : true
+            },
+            studentTool3 : {
+                label : 'tool 3',
+                description : 'this is a tool 3',
+                checked : true
+            },
+            studentTool4 : {
+                label : 'tool 4',
+                description : 'this is a tool 4',
+                checked : false
             }
-        });
+        };
+        
+        $itemPropPanel.append(managerTpl({
+            tools : tools
+        }));
     }
     
     var pciManagerHook = {
         init : function(config){
             
-            console.log(config);
-            return;
+            console.log(config, config.dom.getItemPropertyPanel());
             
-            var $interactionSidebar = config.dom.getInteractionToolbar(),
-                $modalContainer = config.dom.getModalContainer();
-
-            if(interactionsToolbar.isReady($interactionSidebar)){
-                addManagerButton($interactionSidebar, $modalContainer);
-            }else{
-                //wait until the interaction toolbar construciton is done:
-                $interactionSidebar.on('interactiontoolbarready.qti-widget', function(){
-                    addManagerButton($interactionSidebar, $modalContainer);
-                });
-            }
+            var $itemPropPanel = config.dom.getItemPropertyPanel();
             
-            addNewInteractionListener(config.uri);
+            addStudentToolManager($itemPropPanel, config.uri);
 
         }
     };
---
+++ views/js/picManager/tpl/manager.tpl
@@ -0,0 +1,14 @@
+<h2>{{__ "Student tools"}}</h2>
+<ul class="plain">
+    {{#each tools}}
+    <li>
+        <label>
+            <input name="{{@key}}" type="checkbox" {{#if checked}}checked="checked"{{/if}}/>
+                   <span class="icon-checkbox"></span>
+            {{label}}
+        </label>
+        <span class="icon-help tooltipstered" data-tooltip="~ .tooltip-content:first" data-tooltip-theme="info"></span>
+        <span class="tooltip-content">{{description}}</span>
+    </li>
+    {{/each}}
+</ul>
