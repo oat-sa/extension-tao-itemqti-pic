7740aeebcb7c5713694de9314911bfd645353634
Vilmos "Chilly" Kovacs <vilmos@taotesting.com>
2018-01-26 12:36:04 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'QTI Portable Info Control',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '5.1.0',
+    'version' => '5.1.1',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'tao' => '>=12.21.4',
