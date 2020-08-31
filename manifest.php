f4c4c553b56e2e061065c23601492d87c86473ee
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-08-31 16:40:24 +0200

    Version bump

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'QTI Portable Info Control',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '5.6.2',
+    'version' => '5.6.3',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'tao' => '>=23.0.0',
