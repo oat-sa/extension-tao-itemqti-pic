180057d92baccbc43e946608a2949672f2128bc9
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-09-01 14:43:27 +0200

    Migration saved

--- migrations/Version202009011223111298_qtiItemPic.php
+++ migrations/Version202009011223111298_qtiItemPic.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace oat\qtiItemPic\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\qtiItemPic\scripts\install\RegisterPicStudentToolSample;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -15,18 +17,18 @@ final class Version202009011223111298_qtiItemPic extends AbstractMigration
 
     public function getDescription(): string
     {
-        return '';
+        return 'Upgrade studentToolSample 0.4.2 -> 0.4.3';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
+        call_user_func(new RegisterPicStudentToolSample(), ['0.4.3']);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run ' . RegisterPicStudentToolSample::class
+        );
     }
 }
